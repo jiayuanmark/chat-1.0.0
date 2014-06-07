ea75b54feac0c74c613e90a9f9c3573cd0b4965d
jiayuanmark <jiayuanmark@gmail.com>
2014-06-06 19:06:56 -0700

    fix some bugs

--- Chat.hs
+++ Chat.hs
@@ -1,26 +1,27 @@
 -- | CS240h Lab 2 Chat Server
 module Chat (startServer) where
 
-import Network
-import Data.ByteString (ByteString)
-import Data.Text (pack, unpack)
-import Data.Text.Encoding (encodeUtf8, decodeUtf8)
-import System.IO
-import System.Environment (getEnv)
-import Control.Exception
+
 import Control.Concurrent
 import Control.Concurrent.STM
+import Control.Exception
 import Control.Monad
-import Control.Monad.Fix (fix)
+import Data.Functor
+import Network
+import System.Environment (getEnv)
+import System.IO
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
 
 
 type UserName = Int
-type Message = (UserName, ByteString)
+type Message = (UserName, T.Text)
 
 
 -- | Get chat server port.
 readPort :: IO Int
-readPort = liftM read $ getEnv "CHAT_SERVER_PORT"
+readPort = read <$> getEnv "CHAT_SERVER_PORT"
 
 
 -- | Chat server entry point.
@@ -39,33 +40,43 @@ serverLoop s chan user = do
   bracket (accept s) (\(h, _, _) -> hClose h) $ 
     \(h, host, port) -> do
       putStrLn $ "Connection from host " ++ host ++ " port " ++ show port
-      _ <- forkIO $ serveRequest h chan user
+      hSetEncoding h utf8
+      hSetBuffering h LineBuffering
+      void $ forkIO $ serveRequest h chan user
       serverLoop s chan $! user+1
-
-
-encode :: String -> ByteString
-encode = encodeUtf8 . pack
-
-decode :: ByteString -> String
-decode = unpack . decodeUtf8
 
 
 -- | Serve request.
 serveRequest :: Handle -> TChan Message -> UserName -> IO ()
 serveRequest hdl chan user = do
-  let broadcast msg = atomically $ writeTChan chan (user, encode msg)
-  hPutStrLn hdl $ show user ++ " has joined"
-  broadcast $ show user ++ " has joined"
-  chan' <- atomically $ dupTChan chan
-  reader <- forkIO $ fix $ \loop -> do
-  	(usr, line) <- atomically $ readTChan chan'
-  	when (user /= usr) $ hPutStrLn hdl (decode line)
-  	loop
-  handle (\(SomeException _) -> return ()) $ fix $ \loop -> do
-  	line <- liftM init (hGetLine hdl)
-  	broadcast $ show user ++ ": " ++ line
-  	loop
-  killThread reader
-  broadcast $ show user ++ " has left."
-  hClose hdl
+  T.hPutStrLn hdl $ joinMsg user
+  broadcast $ joinMsg user
+  ch <- atomically $ dupTChan chan
+  void $ forkIO $ reader ch
+  writer
+  where
+    broadcast msg' = atomically $ writeTChan chan (user, msg')
+
+    joinMsg user' = T.pack $ show user' ++ " has joined"
+
+    leftMsg user' = T.pack $ show user' ++ " has left"
+
+    chatMsg user' msg' = T.pack $ show user' ++ ": " ++ msg'
+
+    reader chan' = do
+      (usr, line) <- atomically $ readTChan chan'
+      when (user /= usr) $ T.hPutStrLn hdl line
+      reader chan'
+      `catch`
+      ((\_-> hClose hdl) :: SomeException -> IO ())
+
+    writer = do
+      line <- hGetLine hdl
+      broadcast $ chatMsg user line
+      writer
+      `catch`
+      ((\_-> broadcast (leftMsg user) >> hClose hdl) :: SomeException -> IO ())
+
+
+
 
